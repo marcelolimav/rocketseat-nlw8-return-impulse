9c7a1a8271428be0bb62670b6e8d9746d1973751
marcelolimav <marcelolimav@gmail.com>
2022-05-16 06:37:25 -0300

    first commit

---
+++ server/src/adapters/nodemailer/nodemailer-mail-adapter.ts
@@ -0,0 +1,22 @@
+import { MailAdapter, SendMailData } from "../mail-adapter";
+import nodemailer from "nodemailer";
+
+const transport = nodemailer.createTransport({
+  host: "smtp.mailtrap.io",
+  port: 2525,
+  auth: {
+    user: "a1ad8e9e1714b9",
+    pass: "fe4254017cbc12",
+  },
+});
+
+export class NodemailerMailAdapter implements MailAdapter {
+  async sendMail({ subject, body }: SendMailData) {
+    await transport.sendMail({
+      from: "Equipe Feedget <oi@feedget.com>",
+      to: "Marcelo L Veraldo <marcelolimav@gmail.com>",
+      subject,
+      html: body,
+    });
+  }
+}
